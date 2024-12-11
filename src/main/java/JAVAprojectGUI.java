aa730a3d6af7bcdb42c82502cf7abb45fd48c6d8
konradwoehner <konrad.woehner@student.hnu.de>
2024-12-11 20:16:33 +0100

    Help

--- src/main/java/JAVAprojectGUI.java
+++ src/main/java/JAVAprojectGUI.java
@@ -1,6 +1,9 @@
+import org.example.Smartphone;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class JAVAprojectGUI extends JFrame{
     private JLabel titelLabel;
@@ -18,18 +21,25 @@ public class JAVAprojectGUI extends JFrame{
     private JLabel preisLabel;
     private JTextField preisTF;
     private JButton konfigurierenButton;
-    private JTextField textField1;
+    private JTextField gravurTF;
     private JLabel gravurLabel;
     private JButton speichernButton;
     private JLabel intitalenBedingungLabel;
     private JLabel verlaufLabel;
     private JList verlaufList;
+    private JLabel euroLabel;
+    private JTextField verlaufTF;
+
+    private ArrayList<Smartphone> smartphones = new ArrayList<Smartphone>();
+    private String modell, farbe, gravur;
+    private int speicher, ram;
+    private double preis;
 
     public JAVAprojectGUI() {
         setTitle("Smartphone-Konfigurator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocation(450, 220);
-        setSize(900, 600);
+        setLocation(480, 145);
+        setSize(600, 600);
         setContentPane(smartphonePanel);
         setVisible(true);
 
@@ -44,6 +54,28 @@ public class JAVAprojectGUI extends JFrame{
         speichernButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                modell = modellCB.getSelectedItem().toString();
+                farbe = farbeCB.getSelectedItem().toString();
+                speicher = Integer.parseInt(speicherCB.getSelectedItem().toString());
+                ram = Integer.parseInt(ramCB.getSelectedItem().toString());
+                gravur = gravurTF.getText();
+                preis = Double.parseDouble(preisTF.getText().toString());
+
+                Smartphone a = new Smartphone(modell, farbe, gravur, speicher, ram, preis);
+                smartphones.add(a);
+
+
+
+
+                //Versuch Arryliste in Textfeld Verlauf auszugeben, aber Verzweiflung
+                verlaufTF.setText(modell + ", " + farbe + ", " + speicher + "GB, " + ram + "GB, " + gravur + ", " + preis + "€");
+
+                for(Smartphone smartphone: smartphones){
+                    smartphone.ausgeben();
+                    verlaufTF.setText(modell + ", " + farbe + ", " + speicher + "GB, " + ram + "GB, " + gravur + ", " + preis + "€");
+                }
+
+
 
             }
         });
